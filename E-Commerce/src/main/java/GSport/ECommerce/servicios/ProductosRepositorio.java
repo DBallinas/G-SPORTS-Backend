422eb06711efa43b54d366357b98ca02642a1c49
José Luis <joseluisfloresc15@gmail.com>
2022-07-11 03:04:06 -0500

    Se agrega base de datos y se conecta con eclipce

---
+++ E-Commerce/src/main/java/GSport/ECommerce/servicios/ProductosRepositorio.java
@@ -0,0 +1,16 @@
+package GSport.ECommerce.servicios;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import GSport.ECommerce.model.Productos;
+
+public interface ProductosRepositorio extends JpaRepository<Productos, Long>{
+	
+	@Query("SELECT p FROM Productos p WHERE p.NombreProducto=?1")
+	
+	Optional<Productos>findByNombre(String NombreProducto);
+
+}
---
+++ bases de datos g-sports/categorías.sql
@@ -0,0 +1,16 @@
+SELECT * FROM `g-sports`.categorias;
+
+insert into categorias
+VALUES ('Cat1', 'Accesorios','Productos varios deportivos');
+
+insert into categorias
+VALUES ('Cat2', 'Aparatos','Máquinas para hacer ejercicio');
+
+insert into categorias
+VALUES ('Cat3', 'Calzado','Los mejores calzados deportivos');
+
+insert into categorias
+VALUES ('Cat4', 'Ropa','Para vestirte mejor');
+
+insert into categorias
+VALUES ('Cat5', 'Suplementos', 'Mejora tu condición');
